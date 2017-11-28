b63ec889b3d68f4fe2cd9922f60054c3e3c2bcda
hustic <sotgouli@gmail.com>
2017-11-28 17:40:11 +0200

    REST framework ready

--- eBate/liveDebate/models.py
+++ eBate/liveDebate/models.py
@@ -1,22 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db.models.signals import post_init
+#from django.db.models.signals import post_init
 
 from django.db import models
 
-# Create your models here.
-
-
-class DeBox(models.Model):
-	#author = models.ForeignKey(Account)
-	#content = models.TextField()
-
-	#created_at = models.DateTimeField(auto_now_add=True)
-	#update_at = models.DateTimeField(auto_now=True)
-
-	def __unicode__(self):
-		return '{0}'.format(self.content)
 
 """class Debater(models.Model):
     username = models.ForeignKey('auth.User')
